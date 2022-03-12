fd86d6a088f251197240cf634a51d19f4d5d1139
palewire <b@palewi.re>
2022-03-12 11:52:53 -0800

    Added cnn, foxnews, wsj as well as height and width config

--- shoot.py
+++ shoot.py
@@ -4,26 +4,30 @@ import subprocess
 
 
 HANDLE_LIST = csv.DictReader(open("./sources.csv", "r"))
-HANDLE_LOOKUP = dict((d['handle'], d['url']) for d in HANDLE_LIST)
+HANDLE_LOOKUP = dict((d['handle'], d) for d in HANDLE_LIST)
+
+DEFAULT_WIDTH = "1300"
+DEFAULT_HEIGHT = "1600"
+DEFAULT_WAIT = "2000"
 
 
 @click.command()
 @click.argument('handle')
 def main(handle):
-    url = HANDLE_LOOKUP[handle]
+    data = HANDLE_LOOKUP[handle]
     subprocess.run([
         "shot-scraper",
-        url,
+        data['url'],
         "-o",
          f"{handle}.jpg",
         "--quality",
         "80",
         "--width",
-        "1600",
+        data['width'] or DEFAULT_WIDTH,
         "--height",
-        "1600",
+        data['height'] or DEFAULT_HEIGHT,
         "--wait",
-        "2000",
+        data['wait'] or DEFAULT_WAIT,
     ])
 
 
