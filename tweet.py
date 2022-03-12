c2505f4cb2ec6be2479f31db817ea35ce396ce9b
palewire <b@palewi.re>
2022-03-12 12:55:14 -0800

    Added alt text to images

--- tweet.py
+++ tweet.py
@@ -14,8 +14,10 @@ HANDLE_LOOKUP = dict((d['handle'], d['url']) for d in HANDLE_LIST)
 @click.command()
 @click.argument('handle')
 def main(handle):
+    # Pull the source’s metadata
     url = HANDLE_LOOKUP[handle]
 
+    # Connect to Twitter
     api = twitter.Api(
         consumer_key=os.getenv('TWITTER_CONSUMER_KEY'),
         consumer_secret=os.getenv('TWITTER_CONSUMER_SECRET'),
@@ -23,7 +25,22 @@ def main(handle):
         access_token_secret=os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
     )
 
-    api.PostUpdate(f"The @{handle} homepage", media=[f"./{handle}.jpg"])
+    # Create the headline
+    tweet = f"The @{handle} homepage"
+
+    # Get the image
+    image_path = f"./{handle}.jpg"
+    io = temp_file_read = open(image_path, 'rb')
+    media_id = api.UploadMediaSimple(io)
+
+    # Post the media
+    api.PostMediaMetadata(media_id, tweet)
+
+    # Make the tweet
+    api.PostUpdate(
+        tweet,
+        media=media_id,
+    )
 
 
 if __name__ == "__main__":
