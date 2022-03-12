14c0ea37722d0fe4022248f553b2c6ecff3f4d50
palewire <b@palewi.re>
2022-03-12 14:58:02 -0800

    Wrote an archive method, but we're being denied access for now. Gotta bug IA

---
+++ archive.py
@@ -0,0 +1,51 @@
+import csv
+from datetime import datetime
+import os
+
+import click
+import internetarchive
+import pytz
+
+
+HANDLE_LIST = csv.DictReader(open("./sources.csv", "r"))
+HANDLE_LOOKUP = dict((d['handle'], d) for d in HANDLE_LIST)
+
+IA_ACCESS_KEY = os.getenv("IA_ACCESS_KEY")
+IA_SECRET_KEY = os.getenv("IA_SECRET_KEY")
+IA_COLLECTION = os.getenv("IA_COLLECTION")
+
+
+@click.command()
+@click.argument('handle')
+def main(handle):
+    # Pull the source’s metadata
+    data = HANDLE_LOOKUP[handle]
+
+    # Get the timestamp
+    now = datetime.now()
+
+    # Convert it to local time
+    tz = pytz.timezone(data['timezone'])
+    now_local = now.astimezone(tz)
+
+    title = f"The @{data['url']} homepage at {now_local.strftime('%-I:%M %p')} local time"
+
+    identifier = f"{handle}-{now_local.strftime('%s')}"
+    kwargs = dict(
+        files=[open(f"./{handle}.jpg", "rb")],
+        metadata=dict(
+            title=title,
+            collection=IA_COLLECTION,
+            mediatype="image",
+            publisher=data['url'],
+            date=str(now_local),
+            contributor="https://github.com/palewire/news-homepages",
+        ),
+        access_key=IA_ACCESS_KEY,
+        secret_key=IA_SECRET_KEY,
+    )
+    item = internetarchive.upload(identifier, **kwargs)
+
+
+if __name__ == "__main__":
+    main()
