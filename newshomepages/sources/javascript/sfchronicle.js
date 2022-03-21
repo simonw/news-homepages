97f546a206c74c4ffd72564e49bc5fee49f1b876
palewire <b@palewi.re>
2022-03-21 08:02:38 -0700

    Add another sfchronicle ad hit

--- newshomepages/sources/javascript/sfchronicle.js
+++ newshomepages/sources/javascript/sfchronicle.js
@@ -1,3 +1,3 @@
 document.querySelectorAll(
-    '.fancybox-overlay.fancybox-overlay-fixed'
+    '.fancybox-overlay.fancybox-overlay-fixed,.belowMastheadWrapper'
   ).forEach(el => el.style.display = 'none')
