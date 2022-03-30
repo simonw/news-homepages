281779e4b3342cc005669b721b08da86bd3d0696
Ben Welsh <b@palewi.re>
2022-03-30 09:25:19 -0700

    Update thedbk.js

--- newshomepages/sources/javascript/thedbk.js
+++ newshomepages/sources/javascript/thedbk.js
@@ -1,4 +1,3 @@
 document.querySelectorAll(
-    '#div-gpt-ad-1598471439011-0, #div-gpt-ad-1596391450199-0, #panel-375410-0-1-0, #panel-375410-0-1-1'// <-- Pull your page’s identifiers here. If there's more than one thing to target you can comma seperate them.
-
+  '#div-gpt-ad-1598471439011-0,#div-gpt-ad-1596391450199-0,#panel-375410-0-1-0,#panel-375410-0-1-1'
 ).forEach(el => el.style.display = 'none')
