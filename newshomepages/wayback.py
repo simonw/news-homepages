56b5dfac00f96727fe64204789ec391aab1b5c9a
palewire <b@palewi.re>
2022-04-03 17:34:34 -0700

    Do auth connection to wayback

--- newshomepages/wayback.py
+++ newshomepages/wayback.py
@@ -1,10 +1,15 @@
+import os
 import time
+import typing
 
 import click
 import savepagenow
 from savepagenow.exceptions import CachedPage, TooManyRequests, UnknownError
 
 from . import utils
+
+IA_ACCESS_KEY = os.getenv("IA_ACCESS_KEY")
+IA_SECRET_KEY = os.getenv("IA_SECRET_KEY")
 
 
 @click.group()
@@ -20,7 +25,7 @@ def single(handle: str) -> str:
     # Pull the source’s metadata
     site = utils.get_site(handle)
     # Upload it
-    wayback_url = _save_url(site["url"])
+    wayback_url = _curl_url(site["url"])
     if wayback_url:
         click.echo(f"Archived {site['url']} at {wayback_url}")
     return wayback_url
@@ -34,11 +39,28 @@ def bundle(slug: str) -> list:
     url_list = []
     for site in site_list:
         # Upload
-        wayback_url = _save_url(site["url"])
+        wayback_url = _curl_url(site["url"])
         time.sleep(2.5)
         if wayback_url:
             url_list.append([site["handle"], wayback_url])
     return url_list
+
+
+def _curl_url(url):
+    command_list: typing.List[str] = [
+        "curl",
+        "-X",
+        "POST",
+        "-H",
+        "'Accept: application/json'",
+        "-H",
+        f"'Authorization: LOW {IA_ACCESS_KEY}:{IA_SECRET_KEY}'",
+        "-d",
+        f"'url={url}'",
+        "https://web.archive.org/save",
+    ]
+    click.echo(f"Archiving {url}")
+    os.system(" ".join(command_list))
 
 
 def _save_url(url):
