3895e75418e7be0a9f3c9bc5a65d0c8c8cda0740
palewire <b@palewi.re>
2022-04-09 06:49:29 -0700

    Fixed libe ad blocker js

--- newshomepages/sources/javascript/libe.js
+++ newshomepages/sources/javascript/libe.js
@@ -1,3 +1,7 @@
 document.querySelectorAll(
-  '.message-container,[class^="StickyAd"]'
+  '[class^="StickyAd"]'
+).forEach(el => el.style.display = 'none')
+
+document.querySelectorAll(
+  '.message-container,.message-container,[id^="sp_message_container"]'
 ).forEach(el => el.remove())
